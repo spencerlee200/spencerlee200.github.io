df28329ca5f4c2d7e77813f0fcd7b316164d88d8
Spencer Lee <spencer@wirewax.com>
2019-09-03 15:31:34 -0400

    Various updates

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@ $(document).ready(function($) {
   var listItems = $("#work ul li");
   listItems.each(function(i, li) {
     var gradients = [
+                     "linear-gradient(to right, #396afc, #2948ff)",
                      "linear-gradient(to right, #41295a, #2f0743)",
                      "linear-gradient(to left, #ed213a, #93291e)",
+                     "radial-gradient( circle farthest-corner at 10% 20%,  rgba(255,192,0,1) 0%, rgba(246,214,0,1) 62.5% )",
+                     "linear-gradient(to right, #a8e063, #56ab2f)",
                      "linear-gradient(to right, #06beb6, #48b1bf)",
                      "linear-gradient(to left, #00d2ff , #3a7bd5)",
                      "linear-gradient(5deg, #036564, #033649)",
+                     "linear-gradient(to right, #bbd2c5, #536976)",
                      "linear-gradient(to left, #F09819 , #EDDE5D)",
                      "linear-gradient(to left, #EB3349 , #F45C43)",
                      "linear-gradient(to right, #FF512F , #F09819)",
